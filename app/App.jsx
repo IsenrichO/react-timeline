5f7158be75d30f82076a77757014df3e982690af
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-25 21:42:57 -0500

    feat: Pass SeedData as props to Timeline component

--- app/App.jsx
+++ app/App.jsx
@@ -2,9 +2,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import '../assets/styles/master.scss';
+import Timeline from './Timeline';
+import SEED_DATA from './constants/json/SeedData.json';
+
 
 const App = () => (
-  <p>Hello, World!</p>
+  <div id="tl-container">
+    <Timeline
+      data={ SEED_DATA} />
+  </div>
 );
 
 // Inject top-level React component into #root HTML element:
