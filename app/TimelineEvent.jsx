4c3faa7e455a452b091d1ab1541cd8cd2c042bc6
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-25 21:44:24 -0500

    feat: Render given props into respective parts of containing div element

--- app/TimelineEvent.jsx
+++ app/TimelineEvent.jsx
@@ -2,11 +2,24 @@
 import React, { Component } from 'react';
 
 
-const TimelineEvent = () => (
-  <li className="tl-event">
-    <div className="tl-evt-header"></div>
-    <div className="tl-evt-body"></div>
-    <div className="tl-evt-footer"></div>
+const TimelineEvent = ({ evtName, evtLocation, evtAlign, evtDescription, evtNote }) => (
+  <li className={ `tl-event${evtAlign}` }>
+    <div className="tl-marker">
+      <i className="glyphicon glyphicon-record" />
+    </div>
+    <div className="tl-event-panel">
+      <div className="panel-header">
+        <h3>{ evtName }</h3>
+      </div>
+      <div className="panel-body">
+        { evtDescription }
+        <div>
+          <i className="glyphicon glyphicon-map-marker" />
+          <em key={ `Location_${evtLocation}` }>{ evtLocation }</em>
+        </div>
+      </div>
+      <div className="panel-footer">{ evtNote }</div>
+    </div>
   </li>
 );
 
