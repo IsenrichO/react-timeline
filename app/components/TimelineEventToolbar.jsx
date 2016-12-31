f44d6616bc380095f3da1b97c77eb6f212907bac
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-31 14:32:56 -0800

    chore: Add type='button' attribute to button elements

--- app/components/TimelineEventToolbar.jsx
+++ app/components/TimelineEventToolbar.jsx
@@ -4,12 +4,16 @@ import React from 'react';
 
 const TimelineEventToolbar = ({ evt, logModalData, toggleModal }) => (
   <div className="tl-toolbar">
-    <button>
+    <button
+      type="button"
+      name="EditEventBtn">
       <i
         className="glyphicon glyphicon-edit"
         onClick={ () => { logModalData(evt); toggleModal(); } } />
     </button>
-    <button>
+    <button
+      type="button"
+      name="SocialShareBtn">
       <i className="glyphicon glyphicon-share-alt" />
     </button>
   </div>
