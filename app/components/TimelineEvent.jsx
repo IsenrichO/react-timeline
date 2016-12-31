a1e3b9807137b287e3efcc2bfc8c1faddfd14264
Oliver Isenrich <IsenrichO@gmail.com>
2016-12-31 02:17:39 -0800

    chore: Pass event data as props to TimelineEventToolbar

--- app/components/TimelineEvent.jsx
+++ app/components/TimelineEvent.jsx
@@ -4,13 +4,16 @@ import React, { Component } from 'react';
 import TimelineEventToolbar from './TimelineEventToolbar';
 
 
-const TimelineEvent = ({ evtName, evtLocation, evtAlign, evtDescription, evtNote }) => (
+const TimelineEvent = ({ evt, evtName, evtLocation, evtAlign, evtDescription, evtNote, logModalData, toggleModal }) => (
   <li className={ `tl-event${evtAlign}` }>
     <div className="tl-marker">
       <i className="glyphicon glyphicon-record" />
     </div>
     <div className="tl-event-panel">
-      <TimelineEventToolbar />
+      <TimelineEventToolbar
+        evt={ evt }
+        logModalData={ logModalData }
+        toggleModal={ toggleModal } />
       <div className="panel-header">
         <h3>{ evtName }</h3>
       </div>
