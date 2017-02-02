3ab4a43b100cd3c55d98350dd61d402181f03b1d
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-01 20:13:03 -0800

    chore (server): Change sucess temrinal message

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const App = require('./server/router.jsx'),
       PORT = (process.env.PORT || 3000);
 
 App.listen(PORT, () => {
-  console.log(`\
-    Server is up and running on LocalHost at Port ${PORT}:\n\
-    < http://localhost:${PORT}/ >`
+  console.log(`
+============================================================
+  Server is up and running on LocalHost at Port ${PORT}:
+            < http://localhost:${PORT}/ >
+============================================================`
   );
 });
