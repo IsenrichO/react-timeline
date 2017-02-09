21e1a1e8e213c3d487be05c76e9db18f757b7438
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-08 21:04:56 -0800

    refactor (Redux): Delegate action types to own file

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+export const FETCH_SEED_DATA = 'FETCH_SEED_DATA';
+export const LOAD_SEED_DATA = 'LOAD_SEED_DATA';
+export const LOG_EVENT_MODAL_DATA = 'LOG_EVENT_MODAL_DATA';
+export const TOGGLE_EVENT_MODAL = 'TOGGLE_EVENT_MODAL';
