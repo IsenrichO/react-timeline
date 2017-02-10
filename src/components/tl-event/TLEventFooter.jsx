c7b40dc083ca97eef59e119927dd11d7f79d2add
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-09 21:53:53 -0800

    feat (Client): Include tags .glyphicon

--- src/components/tl-event/TLEventFooter.jsx
+++ src/components/tl-event/TLEventFooter.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 
 const TLEventFooter = ({ evtNote }) => (
   <div className="panel-footer">
-    {[
-      evtNote,
-      <i
-        key="StarGlyph"
-        className="glyphicon glyphicon-star-empty" />
-    ]}
+    <div className="evt-tags">
+      {[
+        <i
+          key={ `TagsGlyph` }
+          className="glyphicon glyphicon-tags" />,
+        evtNote
+      ]}
+    </div>
+    <i className="glyphicon glyphicon-star-empty" />
   </div>
 );
 
