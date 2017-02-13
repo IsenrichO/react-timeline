0f2524fd79cd2c9d80a92b27d69f4d1c1cc492d5
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-12 17:38:42 -0800

    chore (DB): Expand EventSchema with new validations

--- db/models/Event.js
+++ db/models/Event.js
@@ -1,28 +1,9 @@
 'use strict';
 const Mongoose = require('mongoose'),
-      Schema = Mongoose.Schema;
+      Schema = Mongoose.Schema,
+      EventSchema = require('../schemas/EventSchema');
 
-
-const EventSchema = new Schema({
-  name: {
-    type: String,
-    validate: {
-      validator: (name) => name.length >= 3,
-      message: 'Your event name must be at least 3 characters long.'
-    },
-    required: [true, 'This event requires a name.']
-  },
-  noteID: String,
-  type: String,
-  description: String,
-  location: String,
-  date: Date,
-  formattedDate: String,
-  photos: Array,
-  photoCount: Number
-}, {
-  collection: 'EventData'
-});
 
 const Event = Mongoose.model('Event', EventSchema);
+
 module.exports = Event;
