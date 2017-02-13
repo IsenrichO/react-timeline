22f271c6354fc9a81d64c65a712b3f51ea62d46b
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-12 20:34:54 -0800

    chore (Redux): Add helpful log statements

--- src/reducers/seedDataAggregator.js
+++ src/reducers/seedDataAggregator.js
@@ -1,14 +1,14 @@
 'use strict';
-import { LOAD_SEED_DATA, ADD_NEW_EVENT } from '../actions/types';
+import { LOAD_SEED_DATA, ADD_NEW_EVENT_DATA } from '../actions/types';
 
 
 export default function seedDataAggregator(state = [], action = null) {
   switch (action.type) {
     case LOAD_SEED_DATA:
-      console.log(`Action ${action.type} executed with payload `, action.payload);
+      console.log(`Action <${action.type}> executed with payload `, action.payload);
       return action.payload;
-    case ADD_NEW_EVENT:
-      console.log(`Action ${action.type} executed with payload `, action.payload);
+    case ADD_NEW_EVENT_DATA:
+      console.log(`Action <${action.type}> executed with payload `, action.payload);
       return new Array(...state).concat(action.payload);
     default:
       return state;
