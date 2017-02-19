c344d019daeef32ffc9e73ed04d2aad161c799c4
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-19 14:53:45 -0800

    chore (Redux): Add newlines between cases for visual clarity

--- src/reducers/batchSelectionItems.js
+++ src/reducers/batchSelectionItems.js
@@ -4,6 +4,7 @@ import { ADD_EVENT_TO_BATCH, CLEAR_BATCH } from '../actions/types';
 
 export default function batchSelectionItems(state = [], action = null) {
   switch (action.type) {
+    
     case ADD_EVENT_TO_BATCH:
       console.log(`Action <${action.type}> executed with payload `, action.payload);
       if (state.includes(action.payload)) {
@@ -13,9 +14,11 @@ export default function batchSelectionItems(state = [], action = null) {
         return newState;
       }
       return state.concat(action.payload);
+      
     case CLEAR_BATCH:
       console.log(`Action <${action.type}> executed with empty payload.`);
       state = [];
+      
     default:
       return state;
   }
