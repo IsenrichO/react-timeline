63add182c0a88ec9cbb4b4ed1bec5b383b6073cd
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-19 13:14:07 -0800

    init (React): Create custom checkbox component for batch selection operations

---
+++ src/components/BatchSelectCheckbox.jsx
@@ -0,0 +1,16 @@
+'use strict';
+import React from 'react';
+
+
+const BatchSelectCheckbox = ({ evtUuid, addSelectionToBatch }) => (
+  <div>
+    <input
+      id={ `select-cb-${evtUuid}` }
+      className="batch-select"
+      type="checkbox"
+      onClick={ () => addSelectionToBatch(evtUuid) } />
+    <label htmlFor={ `select-cb-${evtUuid}` } />
+  </div>
+);
+
+export default BatchSelectCheckbox;
