e30524d3fb190d972994f39f704d0b5ba9b9d228
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-20 14:03:26 -0800

    chore (Routing): Renamed front-end router file

--- src/RouterConfig.jsx
+++ src/RouterConfig.jsx
@@ -3,23 +3,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
-
 import App from './components/pages/App';
 import TimelineEventPage from './components/pages/TimelineEventPage';
 import { fetchSeedData } from './actions/asyncActions';
 import { History, Store } from './store/configureStore';
 
 
-function loadEvts() {
-  Store.dispatch(fetchSeedData());
-}
+const loadEvts = () => Store.dispatch(fetchSeedData());
 
 // 
 const RouterConfig = () => (
   <Provider store={ Store }>
     <Router history={ History }>
-      <Route path="/" component={ App } onEnter={ loadEvts } />
-      <Route path="events/edit/:eventId" component={ TimelineEventPage } />
+      <Route
+        path="/"
+        component={ App }
+        onEnter={ loadEvts } />
+      <Route
+        path="events/edit/:eventId"
+        component={ TimelineEventPage } />
     </Router>
   </Provider>
 );
@@ -27,4 +29,7 @@ const RouterConfig = () => (
 export default RouterConfig;
 
 // Inject router configuration into HTML insertion <div>:
-ReactDOM.render(<RouterConfig />, document.getElementById('root'));
+ReactDOM.render(
+  <RouterConfig />,
+  document.getElementById('root')
+);
