03eb9c187b889e745e4784d89684bfbfdecc0525
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-25 14:09:06 -0800

    client (Routing): Add sidebar routes

--- src/routing/Routes.jsx
+++ src/routing/Routes.jsx
@@ -6,6 +6,10 @@ import { Route, IndexRoute } from 'react-router';
 import App from '../components/pages/App';
 import TimelineEventPage from '../components/pages/TimelineEventPage';
 
+import SearchWrapper from '../components/search/SearchWrapper';
+import AllEvents from '../components/search/AllEvents';
+import StarredEvents from '../components/search/StarredEvents';
+
 // Action creator & middleware imports:
 import { fetchSeedData } from '../actions/asyncActions';
 import { StoreWithMiddleware as Store } from '../store/configureStore';
@@ -19,5 +23,11 @@ export default (
     <Route
       path="events/edit/:eventId"
       component={ TimelineEventPage } />
+    <Route path="search" component={ SearchWrapper }>
+      <IndexRoute component={ AllEvents } />
+      <Route
+        path="starred"
+        component={ StarredEvents } />
+    </Route>
   </Route>
 );
