8823f00049fe00594d7ce1904e3537b38b571946
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-25 14:10:12 -0800

    feat (React): Add hamburger icon and reroute to '/search' on click

--- src/components/pages/App.jsx
+++ src/components/pages/App.jsx
@@ -1,16 +1,20 @@
 'use strict';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
+import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
 import '../../../assets/styles/master.scss';
-import Timeline from '../Timeline';
+import Timeline from '../../containers/Timeline';
 import EditEventModal from '../EditEventModal';
 import { fetchSeedData } from '../../actions/index';
 
 
 @connect(
   (state) => ({ seedData: state.seedDataAggregator }),
-  (dispatch) => bindActionCreators({ fetchSeedData }, dispatch)
+  (dispatch) => bindActionCreators({
+    fetchSeedData,
+    push
+  }, dispatch)
 )
 export default class App extends Component {
   constructor(props) {
@@ -20,6 +24,11 @@ export default class App extends Component {
   render() {
     return (
       <div id="tl-container">
+        <i
+          id="hamburger-ic"
+          onClick={ () => this.props.push('/search') }>
+          &#9776;
+        </i>
         <Timeline seedData={ this.props.seedData } />
         <EditEventModal />
         { this.props.children }
