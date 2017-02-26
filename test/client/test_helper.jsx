3226c84b9b9e6144a1335b100820070a5422fde8
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-25 16:19:59 -0800

    test (Client): Configure DOM-emulator helper utilities

--- test/client/test_helper.jsx
+++ test/client/test_helper.jsx
@@ -2,11 +2,13 @@
 import JSDom from 'jsdom';
 import _$ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import Chai, { expect } from 'chai';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../../src/reducers/index';
+import ChaijQuery from 'chai-jquery';
 
 
 // Set up testing environment to run like browser in the command line:
@@ -25,7 +27,14 @@ function renderComponent(ComponentClass, props, state) {
 }
 
 // Build utility for simulating events:
+$.fn.simulate = function(eventName, value) {
+  if (value) {
+    this.val(value);
+  }
+  TestUtils.Simulate[eventName](this[0]);
+};
 
 // Setup Chai-jQuery:
+ChaijQuery(Chai, Chai.util, $);
 
 export { renderComponent, expect };
