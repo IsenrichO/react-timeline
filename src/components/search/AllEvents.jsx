2686b893f146132463f9f1330cf11708685b1771
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-02 02:23:03 -0800

    feat (React): Ensure ordering is chronological following event updates

--- src/components/search/AllEvents.jsx
+++ src/components/search/AllEvents.jsx
@@ -6,10 +6,9 @@ import { connect } from 'react-redux';
 import SingleEvent from './SingleEvent';
 import { fetchSeedData } from '../../actions/index';
 import { addNewEvent, deleteSingleEvt, updateSingleEvent, deleteBatchEvents, fetchStarredEvents } from '../../actions/asyncActions';
-import * as Utils from '../../Utilities';
+import Utils from '../../utilities/index';
 
 
-// (state) => ({ seedData: state.seedDataAggregator }),
 @connect(
   ({ seedDataAggregator,
      eventEditingModalData, eventEditingModalState,
@@ -42,20 +41,22 @@ export default class AllEvents extends Component {
     return this.props.seedDataAggregator[evt].tags.length > 1;
   }
 
+  renderAllEvents(evts) {
+    return evts.map((evt, index) => (
+      <SingleEvent
+        key={ `EventCard_${index}` }
+        addEventToFavorites={ () => Utils.addEventToFavorites(this.props.updateSingleEvent, evt) }
+        getStarGlyphClass={ ::this.getStarGlyphClass(evt.eventId) }
+        hasMultipleTags={ ::this.hasMultipleTags(evt.eventId) }
+        { ...evt } />
+    ));
+  }
+
   render() {
     const eventData = this.props.seedDataAggregator;
     return (
       <ul className="evt-search">
-        { 
-          eventData.map((evt, index) => (
-            <SingleEvent
-              key={ `EventCard_${index}` }
-              addEventToFavorites={ () => Utils.addEventToFavorites(this.props.updateSingleEvent, evt) }
-              getStarGlyphClass={ ::this.getStarGlyphClass(evt.eventId) }
-              hasMultipleTags={ ::this.hasMultipleTags(evt.eventId) }
-              { ...evt } />
-          ))
-        }
+        { ::this.renderAllEvents(Utils.orderTimelineEvents(eventData)) }
       </ul>
     );
   }
