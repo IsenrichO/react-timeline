579f2698655c1b9285fc44ba604dc16e64250561
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-02 21:12:57 -0800

    init (Routing): Create file holding routes as String-type constants

---
+++ src/routing/RoutePaths.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+export const Events = '/api/events';
+export const SingleEvent = '/api/events/:uuid';
+export const EditEvent = '/api/events/edit/:uuid';
+export const SearchAllEvents = '/api/search';
+export const StarredEvents = '/api/search/starred';
+
+const getSingleEvent = (uuid) => `/api/events/${uuid}`;
+const getEditEvent = (uuid) => `/api/events/edit/${uuid}`;
+
+export { getSingleEvent, getEditEvent };
