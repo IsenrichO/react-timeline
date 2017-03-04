dfba71142525cf7586596e23a69346e538ce7f36
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-03 18:43:08 -0800

    init (Redux): Create new file for proxy action creators

---
+++ src/actions/proxies.js
@@ -0,0 +1,7 @@
+'use strict';
+import Axios from 'axios';
+import * as Types from './types';
+import * as RoutePaths from '../routing/RoutePaths';
+
+
+export const SearchEventsProxy = (...criteria) => {};
