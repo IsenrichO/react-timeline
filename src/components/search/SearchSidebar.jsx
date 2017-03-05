7a9b1728f577a83faaa626d8fb31f5fb1c859c43
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-05 15:23:25 -0800

    refactor (React): Render nav items using map method

--- src/components/search/SearchSidebar.jsx
+++ src/components/search/SearchSidebar.jsx
@@ -4,6 +4,58 @@ import SearchBox from './SearchBox';
 import RangeSlider from '../partials/RangeSlider';
 
 
+const subRoutes = [
+  {
+    routeName: 'All Events',
+    routePath: '/search',
+    routeCategory: 'all',
+    glyphClass: 'calendar',
+    glyph: null,
+    clickHandler: (props) => {
+      props.reroute('/search');
+    },
+    callbackFunc: (props) => null
+  }, {
+    routeName: 'Starred Events',
+    routePath: '/search/starred',
+    routeCategory: 'starred',
+    glyphClass: 'star-empty',
+    glyphClassAlt: 'category-ic',
+    glyph: '&#x2606;',
+    clickHandler: (props) => {
+      props.reroute('/search/starred');
+      props.fetchStarredEvents();
+    },
+    callbackFunc: (props) => props.fetchStarredEvents()
+  }, {
+    routeName: 'Recently Modified',
+    routePath: '/search/recent',
+    routeCategory: 'recently-modified',
+    glyphClass: 'time',
+    glyph: null,
+    clickHandler: (props) => {
+      props.reroute('/search/recent');
+    },
+    callbackFunc: (props) => null
+  }
+];
+
+const renderSearchSubRoutes = (routesArr, props) => routesArr.map((route, index) => (
+  <div
+    key={ `SubRouteCategory_${index}` }
+    className={ `search-category category-${route.routeCategory}` }
+    onClick={ () => route.clickHandler(props) }>
+    <h4>
+      {[
+        route.routeName,
+        <i
+          key={ `SearchSubRouteGlyphicon_${route.glyphClass}` }
+          className={ `glyphicon glyphicon-${route.glyphClass}` } />
+      ]}
+    </h4>
+  </div>
+))
+
 const SearchSidebar = (props) => (
   <div id="sidebar">
     <i
@@ -12,17 +64,9 @@ const SearchSidebar = (props) => (
       &larr;
     </i>
     <SearchBox />
-    <div
-      className="search-category category-starred"
-      onClick={ () => {
-        props.reroute('/search/starred');
-        props.fetchStarredEvents();
-      }}>
-      <h4>
-        Starred
-        <i className="category-ic">&#x2606;</i>
-      </h4>
-    </div>
+
+    { renderSearchSubRoutes(subRoutes, props) }
+
     <div
       className="search-category filter-range"
       // onClick={ () => {
@@ -30,12 +74,32 @@ const SearchSidebar = (props) => (
       // }}
       >
       <h6>Filter By Range</h6>
-
       <RangeSlider />
-
-
     </div>
   </div>
 );
 
 export default SearchSidebar;
+
+
+// <div
+//   className="search-category category-starred"
+//   onClick={ () => {
+//     props.reroute('/search/starred');
+//     props.fetchStarredEvents();
+//   }}>
+//   <h4>
+//     Starred
+//     <i className="category-ic">&#x2606;</i>
+//   </h4>
+// </div>
+// <div
+//   className="search-category category-recently-modified"
+//   onClick={ () => {
+//     props.reroute('/search/recent');
+//   }}>
+//   <h4>
+//     Recent
+//     <i className="glyphicon glyphicon-time" />
+//   </h4>
+// </div>
