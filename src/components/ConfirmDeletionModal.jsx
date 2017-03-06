4d15651a4d40dd9e7aea1a80c4b8d605ee3d36f8
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-05 22:35:49 -0800

    feat (React): Employ a confirmation dialogue for deletion of single events

--- src/components/ConfirmDeletionModal.jsx
+++ src/components/ConfirmDeletionModal.jsx
@@ -1,16 +1,33 @@
 'use strict';
 import React from 'react';
 import Modal from 'react-modal';
+import EventEditingModalStyles from '../constants/json/EventEditingModalStyles.json';
 
 
-const ConfirmDeletionModal = ({ modalStatus }) => (
+const ConfirmDeletionModal = ({ modalStatus, disableModal, confirmDeletionEvt, deleteEvt }) => (
   <Modal
     contentLabel={ `ConfirmDeletionModal_` }
     isOpen={ modalStatus }
     style={ EventEditingModalStyles }
-    ref={ (newEventModal) => { this.newEventModal = newEventModal; }}>
-    <h5>Delete Events&quest;</h5>
-    <p>Are you sure you want to delete these events&quest; This action cannot be undone.</p>
+    // ref={ (confirmDeleteModal) => { this.confirmDeleteModal = confirmDeleteModal; }}
+    >
+    <h5>Delete Event?</h5>
+    <p>Are you sure you want to delete these events? This action cannot be undone.</p>
+    <button
+      type="button"
+      name="cancelEvtConfirmBtn"
+      onClick={ disableModal }>
+      Cancel
+    </button>
+    <button
+      type="button"
+      name="deleteEvtConfirmBtn"
+      onClick={ () => {
+        deleteEvt();
+        disableModal();
+      }}>
+      Delete
+    </button>
   </Modal>
 );
 
