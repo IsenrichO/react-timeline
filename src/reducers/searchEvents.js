d5bc8cb31ec3cf7a29f34a37244ce5d19f78969e
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-06 15:07:38 -0800

    init (Reducer): Create reducer for holding search events

---
+++ src/reducers/searchEvents.js
@@ -0,0 +1,16 @@
+'use strict';
+import { FETCH_RECENTLY_MODIFIED_EVENTS_SUCCESS } from '../actions/types';
+
+
+export default function searchEvents(state = [], action = null) {
+  switch(action.type) {
+
+    case FETCH_RECENTLY_MODIFIED_EVENTS_SUCCESS:
+      console.log(`Action <${action.type}> executed with payload `, action.payload);
+      return action.payload;
+
+    default:
+      console.log(`Action <${action.type}> unrecognized. Falling back to original state.`);
+      return state;
+  }
+};
