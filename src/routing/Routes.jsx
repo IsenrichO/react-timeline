54be0b27f26659fd7473fc6d6516af23b29d24a8
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-06 23:03:38 -0800

    refactor (Routing): Import new SearchSubRoutes nested routes

--- src/routing/Routes.jsx
+++ src/routing/Routes.jsx
@@ -2,18 +2,16 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-// Page-level layout imports:
+// Page-level layout component imports:
 import App from '../components/pages/App';
 import TimelineEventPage from '../components/pages/TimelineEventPage';
 
-import SearchWrapper from '../components/search/SearchWrapper';
-import AllEvents from '../components/search/AllEvents';
-import RecentlyModifiedEvents from '../components/search/RecentlyModifiedEvents';
-import StarredEvents from '../components/search/StarredEvents';
-
 // Action creator & middleware imports:
-import { fetchSeedData, fetchRecentlyModifiedEvents } from '../actions/asyncActions';
+import { fetchSeedData } from '../actions/asyncActions';
 import { StoreWithMiddleware as Store } from '../store/configureStore';
+
+// Child route configuration imports:
+import SearchSubRoutes from './SearchSubRoutes';
 
 
 export default (
@@ -24,19 +22,6 @@ export default (
     <Route
       path="events/edit/:uuid"
       component={ TimelineEventPage } />
-    <Route
-      path="search"
-      component={ SearchWrapper }
-      onEnter={ () => Store.dispatch(fetchSeedData()) }>
-      <IndexRoute component={ AllEvents } />
-      <Route
-        path="recent"
-        component={ RecentlyModifiedEvents }
-        // onEnter={ () => Store.dispatch(fetchRecentlyModifiedEvents()) }
-        />
-      <Route
-        path="starred"
-        component={ StarredEvents } />
-    </Route>
+    { SearchSubRoutes }
   </Route>
 );
