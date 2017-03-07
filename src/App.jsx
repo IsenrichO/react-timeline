638fa2631f1049f0fa95d874a3b129edbf4695f1
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-07 08:35:02 -0800

    feat (HMR): Loading webpack-dev-server (WDS) HMR and instance properties on App parent component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterConfig from './routing/RouterConfig';
 
+if (module.hot) {
+  module.hot.accept()
+}
 
 // Inject router configuration into HTML insertion <div>:
 ReactDOM.render(
