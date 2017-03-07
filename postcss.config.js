e21ce65169862388ac3ac2ce7936884293d28521
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-07 06:56:34 -0800

    config (PostCSS): Instantiate new PostCSS plugins

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,13 @@ module.exports = {
   plugins: [
     require('autoprefixer'),
     require('postcss-import'),
-    require('postcss-nested')
+    require('postcss-nested'),
+    require('postcss-discard-comments')({
+      removeAll: true
+    }),
+    require('postcss-strip-inline-comments'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-selector-matches'),
+    require('postcss-will-change')
   ]
 };
