a62d78c20e94e4ace6d0cd80926149540abc1bc3
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-06 23:02:11 -0800

    chore (React): Omit unnecessary imports and redundant methods

--- src/components/search/SearchWrapper.jsx
+++ src/components/search/SearchWrapper.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
 import SearchSidebar from './SearchSidebar';
-import { updateSingleEvent, fetchStarredEvents, fetchRecentlyModifiedEvents } from '../../actions/asyncActions';
+import { updateSingleEvent } from '../../actions/asyncActions';
 import Utils from '../../utilities/index';
 
 
@@ -12,42 +12,23 @@ import Utils from '../../utilities/index';
   ({ seedDataAggregator, searchEvents }) => ({ seedDataAggregator, searchEvents }),
   (dispatch) => bindActionCreators({
     updateSingleEvent,
-    fetchStarredEvents,
-    fetchRecentlyModifiedEvents,
     push
   }, dispatch)
 )
 export default class SearchWrapper extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      starredEvts: []
-    };
-    this.props.fetchRecentlyModifiedEvents();
-  }
-
-  fetchStarredEvents() {
-    let starredEvts = this.props.seedDataAggregator.filter(evt => evt.starred === true);
-    this.setState({ starredEvts });
   }
 
   delegateAsyncCallback(path) {
     const pathRoute = path.replace(/^\/search\//, '');
-    // const pathsObj = {
-    //   '': null,
-    //   'starred': () => this.props.fetchStarredEvents(),
-    //   'recent': this.props.fetchRecentlyModifiedEvents
-    // };
-    // pathRoute in pathsObj ? pathsObj[pathRoute]() : null;
     return this.props.searchEvents;
   }
 
   render() {
     return (
       <div>
-        <SearchSidebar
-          reroute={ (path) => this.props.push(`${path}`) }
-          fetchStarredEvents={ () => ::this.fetchStarredEvents() } />
+        <SearchSidebar reroute={ (path) => this.props.push(`${path}`) } />
         <main id="search-main">
           {
             cloneElement(this.props.children, {
