4760192d15ddce70673e5c947cfa19ceab93d86f
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-09 05:47:50 -0800

    refactor (React): Comment-out React-DOM render statement

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterConfig from './routing/RouterConfig';
 
-if (module.hot) {
-  module.hot.accept()
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 // Inject router configuration into HTML insertion <div>:
-ReactDOM.render(
-  RouterConfig,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   RouterConfig,
+//   document.getElementById('root')
+// );
