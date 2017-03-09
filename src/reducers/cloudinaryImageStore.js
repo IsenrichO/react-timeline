a2e9b49e7f31a9c14fdfc98f0508ccdb6630e9e0
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-09 05:40:34 -0800

    init (Reducer): Create reducer for managing Cloudinary images

---
+++ src/reducers/cloudinaryImageStore.js
@@ -0,0 +1,18 @@
+'use strict';
+import { FETCH_CLOUDINARY_IMAGES } from '../actions/types';
+
+
+export default function cloudinaryImageStore(state = [], action = null) {
+  switch (action.type) {
+
+    case FETCH_CLOUDINARY_IMAGES:
+      console.log(`Action <${action.type}> executed with payload`, action.payload);
+      console.log('PAYLOAD:\n', action.payload);
+      return action.payload.resources;
+
+    default:
+      console.log(`Action <${action.type}> executed with payload`, action.payload);
+      return state;
+
+  }
+};
