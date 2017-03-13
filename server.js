5a8c28b3f1de8337c61cf4e57ecf5e1cd0d86635
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-13 06:18:20 -0700

    config (Cloudinary): Load in dotenv and initialize Cloudinary configuration

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 'use strict';
 const App = require('./server/app.js'),
-      PORT = (process.env.PORT || 3000);
+      dotenv = require('dotenv').config(),
+      Cloudinary = require('cloudinary').v2;
 
+
+Cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 App.get('*', (req, res) => {
   res.redirect('/');
 });
 
-App.listen(PORT, () => {
+App.listen(process.env.PORT || 3000, () => {
+  console.log('PROCESS: \n\n\n', process.env);
   console.log(`
 ============================================================
   Server is up and running on LocalHost at Port ${PORT}:
