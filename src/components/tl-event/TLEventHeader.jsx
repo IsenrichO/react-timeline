44ccaf2b3b5ce494e2f2f0a27f368a5e9f0bcf2d
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-15 20:25:49 -0700

    refactor (React): Correct inline style declaration

--- src/components/tl-event/TLEventHeader.jsx
+++ src/components/tl-event/TLEventHeader.jsx
@@ -7,7 +7,8 @@ const urlBase = 'http://res.cloudinary.com/http-isenrich-io/image/upload/';
 const TLEventHeader = ({ evtName, evtUuid, batchSelectionState, addSelectionToBatch, inverted, imageData }) => (
   <div
     className={ `panel-header${inverted ? ' inverted' : ''}` }
-    style={{ backgroundImage: imageData ? `url(${urlBase}${imageData.public_id})` : 'none' }}>
+    // style={{ backgroundImage: imageData ? `url(${urlBase}${<encodeURI><imageData class="public_id"></imageData></encodeURI>})` : 'none' }}
+    style={{ backgroundImage: imageData ? `url(${imageData.images[0].secure_url})` : 'none' }}>
     { Utils.renderItemActionControl(batchSelectionState, evtUuid, addSelectionToBatch) }
     <h3>{ evtName }</h3>
   </div>
