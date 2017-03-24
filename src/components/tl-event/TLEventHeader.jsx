1731085e695e55ce1dfb628f4caa547486e17ca9
Oliver Isenrich <IsenrichO@gmail.com>
2017-03-23 19:35:08 -0700

    refactor (React): Check for chosen bg image or use first upload as default

--- src/components/tl-event/TLEventHeader.jsx
+++ src/components/tl-event/TLEventHeader.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import Utils from '../../utilities/index';
 
 
-const urlBase = 'http://res.cloudinary.com/http-isenrich-io/image/upload/';
+const urlBase = 'http://res.cloudinary.com/http-isenrich-io/image/upload/',
+      fetchHeroImage = (images) => (images.some(img => img.hasOwnProperty('isHeroImg'))
+        ? images.find(img => !!img.isHeroImg)
+        : images[0]).secure_url;
 const TLEventHeader = ({ evtName, evtUuid, batchSelectionState, addSelectionToBatch, inverted, imageData }) => (
   <div
     className={ `panel-header${inverted ? ' inverted' : ''}` }
     // style={{ backgroundImage: imageData ? `url(${urlBase}${<encodeURI><imageData class="public_id"></imageData></encodeURI>})` : 'none' }}
-    style={{ backgroundImage: imageData ? `url(${imageData.images[0].secure_url})` : 'none' }}>
+    style={{ backgroundImage: imageData ? `url(${fetchHeroImage(imageData.images)})` : 'none' }}>
     { Utils.renderItemActionControl(batchSelectionState, evtUuid, addSelectionToBatch) }
     <h3>{ evtName }</h3>
   </div>
