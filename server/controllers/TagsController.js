7eea0ca31f670daa393de2a18fc7fbf447541510
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-04 07:29:33 -0700

    init (Controller): Create function for fetching tags from DB

---
+++ server/controllers/TagsController.js
@@ -0,0 +1,19 @@
+'use strict';
+const Tag = require('../../db/models/EventTag');
+
+
+const fetchAllTags = (req, res, next) => {
+  Tag
+    .find({})
+    .sort({ name: 'asc' })
+    .limit(700)
+    .then(tags => res.json(tags))
+    .catch(err => {
+      res.status(400).send('Failed to fetch requested events!');
+      next();
+    });
+};
+
+module.exports = {
+  fetchAllTags
+};
