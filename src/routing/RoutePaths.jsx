33892cd4c63178718d13da856af4985a0935c144
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-04 07:22:38 -0700

    chore (Routing): Include route path for 'tags' BE EP

--- src/routing/RoutePaths.jsx
+++ src/routing/RoutePaths.jsx
@@ -8,6 +8,7 @@ export const AllEvents = '/api/search/';
 export const RecentlyModifiedEvents = '/api/search/recent';
 export const StarredEvents = '/api/search/starred';
 export const Photos = '/api/photos';
+export const Tags = '/api/tags';
 
 const getSingleEvent = (uuid) => `/api/events/${uuid}`;
 const getEditEvent = (uuid) => `/api/events/edit/${uuid}`;
