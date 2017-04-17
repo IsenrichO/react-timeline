49b1032a87d7ce5366d57b85d322a7c232cb7a27
Oliver Isenrich <IsenrichO@gmail.com>
2017-04-17 01:20:11 -0700

    chore (Reducer): Remove extraneous log statements

--- src/reducers/cloudinaryImageStore.js
+++ src/reducers/cloudinaryImageStore.js
@@ -9,9 +9,8 @@ export default function cloudinaryImageStore(state = [], action = null) {
   switch (action.type) {
 
     case FETCH_CLOUDINARY_IMAGES_SUCCESS:
-      console.log(`Action <${action.type}> executed with payload`, action.payload);
+      // console.log(`Action <${action.type}> executed with payload`, action.payload);
       const { subfolderNames = [], tlImages } = action.payload;
-      console.log('SUB-FOLDERNAMES:', subfolderNames, tlImages);
       let foldersObj = subfolderNames.reduce((memo, curr) => {
         const regTest = new RegExp(`^${curr.path}`),
               images = tlImages.filter(img => regTest.test(img.public_id));
@@ -19,25 +18,20 @@ export default function cloudinaryImageStore(state = [], action = null) {
         return memo;
       }, {});
       let newState = Object.assign({}, state, foldersObj);
-      console.log('newState:', newState);
       return newState;
 
     case SET_BCKG_IMAGE:
-      console.log(`Action <${action.type}> executed with payload`, action.payload);
-
+      // console.log(`Action <${action.type}> executed with payload`, action.payload);
       const folderPath = action.payload.replace(/^.+\/React-Timeline\/(.+)\/.+$/, '$1');
       const newFolderImages = state[folderPath].images.map(img => img.secure_url === action.payload
           ? Object.assign({}, img, { isHeroImg: true })
           : img);
-      console.log('NEW FOLDER IMAGES:', newFolderImages);
       const newFolderPath = Object.assign({}, state[folderPath], { images: newFolderImages });
-      console.log('NEW FOLDER PATH:', newFolderPath);
       let newState2 = Object.assign({}, state, { [folderPath]: newFolderPath });
-      console.log('NEW FOLDERIZED STATE:', newState2);
       return newState2;
 
     default:
-      console.log(`Action <${action.type}> executed with payload`, action.payload);
+      // console.log(`Action <${action.type}> executed with payload`, action.payload);
       return state;
   }
 };
